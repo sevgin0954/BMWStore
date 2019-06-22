176db2114b6aac1200ea196a58e74782c556ef68
sevgin0954 <skateboardista_0954@abv.bg>
2019-06-22 17:33:31 +0300

    Add project files.

---
+++ BMWStore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BMWStore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
