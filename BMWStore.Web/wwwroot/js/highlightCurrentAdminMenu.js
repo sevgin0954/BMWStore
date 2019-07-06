2b5dd969b6f005278937a019cd7b5f1db4c0dcf4
sevgin0954 <skateboardista_0954@abv.bg>
2019-07-06 23:48:03 +0300

    Added admin views

---
+++ BMWStore.Web/wwwroot/js/highlightCurrentAdminMenu.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    var menuItems = $('#admin-menu>li>a');
+    let currentActionName = getActionNameFromUrl();
+
+    for (let i = 0; i < menuItems.length; i++) {
+        let currentMenuItem = $(menuItems[i]);
+        let currentItemNameAttr = currentMenuItem.attr('data-name');
+
+        if (currentActionName === currentItemNameAttr) {
+            currentMenuItem.addClass('btn-light-blue');
+        }
+        else {
+            currentMenuItem.removeClass('btn-light-blue');
+        }
+    }
+
+    function getActionNameFromUrl() {
+        let urlParts = document.URL.split("/").slice(-2);
+
+        let actionNameIndex = urlParts.length - 1;
+        let actionName = urlParts[actionNameIndex];
+        while (actionName === '') {
+            actionNameIndex--;
+            actionName = urlParts[actionNameIndex];
+        }
+
+        return actionName;
+    }
+});
