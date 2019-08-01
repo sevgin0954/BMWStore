b58de9fa446f3ee39a37ddccdcce0d876d1b0711
sevgin0954 <skateboardista_0954@abv.bg>
2019-08-01 21:45:23 +0300

    Added tests for AdminModelTypeService

---
+++ BMWStore.Services.Tests/AdminServicesTests/AdminModelTypesServiceTests/DeleteAsyncTests.cs
@@ -0,0 +1,39 @@
+﻿using BMWStore.Common.Constants;
+using System;
+using Xunit;
+
+namespace BMWStore.Services.Tests.AdminServicesTests.AdminModelTypesServiceTests
+{
+    public class DeleteAsyncTests : BaseAdminModelTypesServiceTest, IClassFixture<BaseTestFixture>
+    {
+        private readonly BaseTestFixture baseTest;
+
+        public DeleteAsyncTests(BaseTestFixture baseTest)
+        {
+            this.baseTest = baseTest;
+        }
+
+        [Fact]
+        public async void WithIncorrectId_ShouldTrowException()
+        {
+            var dbContext = this.baseTest.GetDbContext();
+            var service = this.GetService(dbContext);
+            var incorrectId = Guid.NewGuid().ToString();
+
+            var exception = await Assert.ThrowsAsync<ArgumentException>(async () => await service.DeleteAsync(incorrectId));
+            Assert.Equal(ErrorConstants.IncorrectId, exception.Message);
+        }
+
+        [Fact]
+        public async void WithCorrectId_ShouldDeleteModelType()
+        {
+            var dbContext = this.baseTest.GetDbContext();
+            var fuelType = this.SeedModelType(dbContext);
+            var service = this.GetService(dbContext);
+
+            await service.DeleteAsync(fuelType.Id);
+
+            Assert.Empty(dbContext.FuelTypes);
+        }
+    }
+}
---
+++ BMWStore.Services.Tests/AdminServicesTests/AdminModelTypesServiceTests/GetAllAsyncTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace BMWStore.Services.Tests.AdminServicesTests.AdminModelTypesServiceTests
+{
+    public class GetAllAsyncTests : BaseAdminModelTypesServiceTest, IClassFixture<BaseTestFixture>
+    {
+        private readonly BaseTestFixture baseTest;
+
+        public GetAllAsyncTests(BaseTestFixture baseTest)
+        {
+            this.baseTest = baseTest;
+        }
+
+        [Fact]
+        public async void WithoutModelTypes_ShouldReturnEmptyCollection()
+        {
+            var dbContext = this.baseTest.GetDbContext();
+            var service = this.GetService(dbContext);
+
+            var models = await service.GetAllAsync();
+
+            Assert.Empty(models);
+        }
+
+        [Fact]
+        public async void WithModelType_ShouldReturnModelType()
+        {
+            var dbContext = this.baseTest.GetDbContext();
+            var service = this.GetService(dbContext);
+            this.SeedModelType(dbContext);
+
+            var models = await service.GetAllAsync();
+
+            Assert.Single(models);
+        }
+    }
+}
