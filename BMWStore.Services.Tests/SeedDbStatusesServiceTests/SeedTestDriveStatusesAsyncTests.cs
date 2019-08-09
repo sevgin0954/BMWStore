1f8155462e4522e91bc56c2ae572b9fdee0960f7
sevgin0954 <skateboardista_0954@abv.bg>
2019-08-10 02:32:50 +0300

    Added tests for SeedDbStatusesService

--- BMWStore.Services.Tests/SeedDbStatusesServiceTests/SeedTestDriveStatusesAsyncTests.cs
+++ BMWStore.Services.Tests/SeedDbStatusesServiceTests/SeedTestDriveStatusesAsyncTests.cs
@@ -1,9 +1,38 @@
-﻿using Xunit;
+﻿using BMWStore.Common.Enums;
+using BMWStore.Services.Tests.Common.SeedTestMethods;
+using System.Linq;
+using Xunit;
 
 namespace BMWStore.Services.Tests.SeedDbStatusesServiceTests
 {
     public class SeedTestDriveStatusesAsyncTests : BaseSeedDbStatusesServiceTest
     {
+        [Fact]
+        public async void WithStatuses_ShouldAddOnlyNotExistingStatuses()
+        {
+            var dbContext = this.GetDbContext();
+            var dbStatus = SeedStatusesMethods.SeedStatus(dbContext, TestDriveStatus.Canceled);
+            var upcomingStatusName = TestDriveStatus.Upcoming.ToString();
+            var service = this.GetService(dbContext);
 
+            await service.SeedTestDriveStatusesAsync(dbStatus.Name, upcomingStatusName);
+
+            Assert.Equal(2, dbContext.Statuses.Count());
+            Assert.Contains(dbContext.Statuses, s => s.Name == dbStatus.Name);
+            Assert.Contains(dbContext.Statuses, s => s.Name == upcomingStatusName);
+        }
+
+        [Fact]
+        public async void WithAlreadyExistingStatus_ShouldDoNothing()
+        {
+            var dbContext = this.GetDbContext();
+            var dbStatus = SeedStatusesMethods.SeedStatus(dbContext, TestDriveStatus.Canceled);
+            var service = this.GetService(dbContext);
+
+            await service.SeedTestDriveStatusesAsync(dbStatus.Name);
+
+            Assert.Equal(1, dbContext.Statuses.Count());
+            Assert.Contains(dbContext.Statuses, s => s.Name == dbStatus.Name);
+        }
     }
 }
